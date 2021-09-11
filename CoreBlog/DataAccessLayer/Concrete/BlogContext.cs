72eae455fe53a9a1dfa3e53f0a27ea62f024c8e9
Sinan ÖNGEL <songel66@gmail.com>
2021-09-11 11:48:51 +0300

    Ders 9-12

---
+++ CoreBlog/DataAccessLayer/Concrete/BlogContext.cs
@@ -0,0 +1,24 @@
+﻿using EntitiyLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class BlogContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-5S4H6I7; database=CoreBlogDb; integrated security=true;"); 
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
