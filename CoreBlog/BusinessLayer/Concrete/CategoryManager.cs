9e49cd924f892d46ca26a7cfa088913a2128ee55
Sinan ÖNGEL <songel66@gmail.com>
2021-09-18 15:50:15 +0300

    Ders 17-20

---
+++ CoreBlog/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EFCategoryRepository eFCategoryRepository;
+        public CategoryManager()
+        {
+            eFCategoryRepository = new EFCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            eFCategoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            eFCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            eFCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return eFCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return eFCategoryRepository.GetListAll();
+        }
+    }
+}
