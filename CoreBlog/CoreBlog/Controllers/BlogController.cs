b6614070ea74d8cdda5c46dc089b977d06521ab2
Sinan ÖNGEL <songel66@gmail.com>
2021-10-02 15:23:34 +0300

    Ders 21-24

---
+++ CoreBlog/CoreBlog/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogRepository());
+        public IActionResult Index()
+        {
+            var values = blogManager.GetList();
+            return View(values);
+        }
+    }
+}
