606d5112636c3f951bff57d49b3280fb331c6e85
Sinan ÖNGEL <songel06@hotmail.com>
2021-10-17 16:27:17 +0300

    Ders 43-46

--- CoreBlog/CoreBlog/Controllers/LoginController.cs
+++ CoreBlog/CoreBlog/Controllers/LoginController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntitiyLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,26 @@ namespace CoreBlog.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Index(Writer p)
+        {
+            BlogContext blogContext = new BlogContext();
+            var dataValue = blogContext.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPasword == p.WriterPasword);
+            if (dataValue != null)
+            {
+                HttpContext.Session.SetString("username", p.WriterMail);
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
