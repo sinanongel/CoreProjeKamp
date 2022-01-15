f575b09d06696e1f5d9204e3cda9b509d4769884
Sinan ÖNGEL <songel06@gmailcom>
2022-01-15 13:20:06 +0300

    Ders 70-73

--- CoreBlog/CoreBlog/Controllers/DashboardController.cs
+++ CoreBlog/CoreBlog/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,10 @@ namespace CoreBlog.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            BlogContext context = new BlogContext();
+            ViewBag.v1 = context.Blogs.Count().ToString();
+            ViewBag.v2 = context.Blogs.Where(x => x.WriterID == 1).Count();
+            ViewBag.v3 = context.Categories.Count();
             return View();
         }
     }
--- CoreBlog/CoreBlog/Controllers/WriterController.cs
+++ CoreBlog/CoreBlog/Controllers/WriterController.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using CoreBlog.Models;
+using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +16,7 @@ namespace CoreBlog.Controllers
 {
     public class WriterController : Controller
     {
+        WriterManager writerManager = new WriterManager(new EFWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -22,13 +30,69 @@ namespace CoreBlog.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
         public PartialViewResult WriterNavbarPartial()
         {
             return PartialView();
         }
+        [AllowAnonymous]
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writerValues = writerManager.TGetById(1);
+            return View(writerValues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult result = wv.Validate(p);
+            if (result.IsValid)
+            {
+                writerManager.TUpdate(p);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer writer = new Writer();
+            if (p.WriterImage != null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                writer.WriterImage = newImageName;
+            }
+            writer.WriterMail = p.WriterMail;
+            writer.WriterName = p.WriterName;
+            writer.WriterPasword = p.WriterPasword;
+            writer.WriterAbout = p.WriterAbout;
+            writerManager.TAdd(writer);
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
