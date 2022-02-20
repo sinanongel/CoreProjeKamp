4116aa963cae6f097a831a1c9d7d7d6aed8d4da0
Sinan ÖNGEL <songel06@gmailcom>
2022-02-20 20:18:02 +0300

    Ders  91-94

--- CoreBlog/CoreBlog/Areas/Admin/Controllers/CategoryController.cs
+++ CoreBlog/CoreBlog/Areas/Admin/Controllers/CategoryController.cs
@@ -1,17 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreBlog.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
+        public IActionResult Index(int page = 1)
+        {
+            var values = categoryManager.GetList().ToPagedList(page, 3);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidator validationRules = new CategoryValidator();
+            ValidationResult result = validationRules.Validate(p);
+
+            if (result.IsValid)
+            {
+                p.CategoryStatus = true;
+                categoryManager.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else if (!result.IsValid)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = categoryManager.TGetById(id);
+            categoryManager.TDelete(value);
+            return RedirectToAction("Index");
         }
     }
 }
