77338bbf39c6ca4c634a933ec4770381efc2b793
Sinan ÖNGEL <songel06@gmailcom>
2022-03-12 12:43:32 +0300

    Ders  106-109

---
+++ CoreBlog/CoreBlog/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,49 @@
+﻿using CoreBlog.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Areas.Admin.Controllers
+{   
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+        public IActionResult GetWriterByID(int writerId)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerId);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Fatma"
+            },
+            new WriterClass
+            {
+                Id=2,
+                Name="Ali"
+            },
+            new WriterClass
+            {
+                Id=3,
+                Name="Ebuzer"
+            }
+        };
+    }
+}
