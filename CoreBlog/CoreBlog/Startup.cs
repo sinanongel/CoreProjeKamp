9cc47aadd9e989c4f9c80b02e7751225b1c63992
Sinan ÖNGEL <songel06@gmailcom>
2022-04-03 18:03:13 +0300

    Ders 122-123

--- CoreBlog/CoreBlog/Startup.cs
+++ CoreBlog/CoreBlog/Startup.cs
@@ -1,9 +1,11 @@
+using EntitiyLayer.Concrete;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace CoreBlog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbContext>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<DbContext>();
             services.AddControllersWithViews();
 
             //services.AddSession();
--- CoreBlog/DataAccessLayer/Concrete/BlogContext.cs
+++ CoreBlog/DataAccessLayer/Concrete/BlogContext.cs
@@ -1,4 +1,5 @@
 ﻿using EntitiyLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class BlogContext : DbContext
+    public class BlogContext : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -29,6 +30,8 @@ namespace DataAccessLayer.Concrete
                 .WithMany(y => y.WriterReceiver)
                 .HasForeignKey(z => z.ReceiverID)
                 .OnDelete(DeleteBehavior.ClientSetNull);
+
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Blog> Blogs { get; set; }
